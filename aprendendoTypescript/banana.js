54de385a8059879b911eb8c5b5fb34eee65fe06c
saocebri <albacarlos2012@gmail.com>
2022-03-21 23:23:06 -0300

    Teste de Commit individul

--- aprendendoTypescript/banana.js
+++ aprendendoTypescript/banana.js
@@ -7,6 +7,9 @@ var p1: number = 110
 var p2: number = 11.35
 var total = p1 + p2
 
+
+//teste de git
+
 console.log(`Boa noite sr. ${nome}, você tirou ${p1} na p1 e ${p2} na p2 e a sua pontuação total é ${total}`) */
 /* var num1 = 2 // inferindo o tipo do dado da variável
 console.log("O valor da variável num1 é: " + num1) */
